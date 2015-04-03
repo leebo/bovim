0aeed1d88c4ea15c71576561e28bb2b75830b021
leebo <me@bobo.im>
2015-04-03 13:03:24 +0800

    更新linux 初始化脚本

--- init.sh
+++ init.sh
@@ -1,13 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 git submodule init
 git submodule update
 
 if [ ! -d "~/.vim/" ]; then
+  echo "not have .vim"
+else
   rm -Rf ~/.vim.old
   mv ~/.vim ~/.vim.old
 fi
 
 if [ ! -f "~/.vimrc" ]; then
+  echo "not have .vimrc"
+else
   rm ~/.vimrc.old
   mv ~/.vimrc ~/.vimrc.old
 fi
